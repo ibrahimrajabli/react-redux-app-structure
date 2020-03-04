f3b5f3e28ff666c6f0c4579df67406675e8b25e8
ibrahimrajabli <rajabli@live.com>
2020-03-04 14:07:01 +0300

    first commit

--- src/pages/home/mainpage.js
+++ src/pages/home/mainpage.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
+import * as actionTypes from "../../redux/actionTypes"
 
 class Mainpage extends Component {
+    someTest(){
+        var newSome = this.props.mainReducer.someData
+        newSome.name = "test";
+        this.props.dispatch({type:actionTypes.SOME_ACTION_TYPE,payload:newSome})
+        console.log(this.props.mainReducer)
+    }
     render() {
         return (
             <div>
-                test here
+                <button onClick={()=>this.someTest()}>test but</button>
+                {this.props.mainReducer.someData.name}
             </div>
         );
     }
--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -1,11 +1,11 @@
 import * as actionTypes from "./actionTypes"
 
-const initialState = {someData:{}};
+const initialState = {someData:{"name":"name1"}};
 
 const mainReducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.SOME_ACTION_TYPE:
-            return {...state, someData: action.payload};
+            return {...state, name: action.payload};
         default:return state
     }
 };
